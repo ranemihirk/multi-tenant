a7b1009a3e385a83e3c4880794b361a24e2dd831
Mihir Rane <ranemihirk@gmail.com>
2025-09-23 15:09:42 +0530

    admin page added

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from "next/image";
+import { useTenantContext } from "@/app/contexts/TenantContext";
+// import * as Styles from './styles';
+
+export default function Home() {
+  const { tenant } = useTenantContext();
+
+  return (
+    <header></header>
+  );
+}
