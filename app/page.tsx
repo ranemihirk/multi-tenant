abf168f0d90b563aa872786760215ae1008103fa
Mihir Rane <ranemihirk@gmail.com>
2025-10-07 18:35:15 +0530

    Public homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,17 @@ import Image from "next/image";
 import { useTenantContext } from "@/app/contexts/TenantContext";
 
 export default function Home() {
-  const { tenant } = useTenantContext();
+  // const { tenant } = useTenantContext();
 
   return (
     <main className="p-8">
-      <h1 className="text-3xl font-bold">
+      {/* <h1 className="text-3xl font-bold">
         🎬 {tenant ?? "No Tenants"} Production House SaaS
       </h1>
       <p className="mt-4 text-gray-600">
         Manage shoots, crew, equipment, and expenses — all in one place.
-      </p>
+      </p> */}
+      Public Homepage
     </main>
   );
 }
